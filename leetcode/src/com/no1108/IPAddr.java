75738bcf2cfcf457f94c13dfcfa4accd27f28994
zhangyang891008 <zhangyang891008@gmail.com>
2019-10-18 21:52:39 +0800

    IP地址无效、移除外层括号、转小写

---
+++ leetcode/src/com/no1108/IPAddr.java
@@ -0,0 +1,45 @@
+package com.no1108;
+
+/*
+1108Ip地址无效化
+给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+
+所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+ 
+
+示例 1：
+
+输入：address = "1.1.1.1"
+输出："1[.]1[.]1[.]1"
+示例 2：
+
+输入：address = "255.100.50.0"
+输出："255[.]100[.]50[.]0"
+ 
+
+提示：
+
+给出的 address 是一个有效的 IPv4 地址
+
+
+ */
+public class IPAddr {
+    public String defangIPaddr(String address) {
+        if(address == null)
+            return null;
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < address.length(); i++) {
+            if(address.charAt(i) == '.')
+                s.append('[').append(address.charAt(i)).append(']');
+            else
+                s.append(address.charAt(i));
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        String address = "11.22.44.21";
+        String s = new IPAddr().defangIPaddr(address);
+        System.out.println(s);
+    }
+}
