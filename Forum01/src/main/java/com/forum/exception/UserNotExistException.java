abb066a1daef2909c1dfe44024cc64f96acf97b9
zhangyang891008 <49987792+zhangyang891008@users.noreply.github.com>
2020-03-28 11:25:58 +0800

    Revert "论坛项目-用户注册、登陆、锁定解锁"
    This reverts commit 5a1ee80e1757a70698fcc8bdca6709b64c352647.

--- Forum01/src/main/java/com/forum/exception/UserNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.forum.exception;
-
-public class UserNotExistException extends Exception{
-    public UserNotExistException(String username){
-        super("user not exist:"+username);
-    }
-}
--- Forum01/src/main/resources/sql/board_manager.sql
+++
@@ -1,5 +0,0 @@
-<!--t_board_manager表-->
-CREATE TABLE t_board_manager(
-    board_id int,
-    user_id int
-)engine = InnoDB DEFAULT CHARSET = utf8;
