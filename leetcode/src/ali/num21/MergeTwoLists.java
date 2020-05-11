96c0b8112e8ef8b99381bc926df6971acd89dd84
zhangyangt <zhangyangt@yonyou.com>
2020-05-11 16:23:24 +0800

    LeetCode：添加另外的简洁写法

--- leetcode/src/ali/num21/MergeTwoLists.java
+++ leetcode/src/ali/num21/MergeTwoLists.java
@@ -56,7 +56,38 @@ public class MergeTwoLists {
         return root.next;
     }
 
-    public static void main(String[] args) {
+    /*
+    方法2：题解里的写法
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
 
+        if(l2 == null){
+            return l1;
+        }
+
+        ListNode result = new ListNode(-1);
+        ListNode pre = result;
+
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                pre.next = l1;
+                l1 = l1.next;
+            }else{
+                pre.next= l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+
+        pre.next = ( l1==null?l2:l1);
+        return result.next;
+
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
