16d5138407049b637bf49f3da1836111ad341751
zhangyangt <zhangyangt@yonyou.com>
2020-05-20 11:20:22 +0800

    LeetCode：139. 单词拆分 -- 动态规划**

---
+++ leetcode/src/ali/num139/WordBreak.java
@@ -0,0 +1,64 @@
+package ali.num139;
+
+import com.sun.corba.se.spi.orbutil.threadpool.Work;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/*
+139. 单词拆分
+
+给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+
+说明：
+
+拆分时可以重复使用字典中的单词。
+你可以假设字典中没有重复的单词。
+示例 1：
+
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+示例 2：
+
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+     注意你可以重复使用字典中的单词。
+示例 3：
+
+输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: false
+ */
+public class WordBreak {
+    /**
+     * 动态规划
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length()+1];
+
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if(dp[j] && words.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        String s = "catsandog";
+        List<String> wordDict = Arrays.asList("cats", "dog", "sand", "and", "cat");
+        boolean b = new WordBreak().wordBreak(s, wordDict);
+        System.out.println(b);
+
+    }
+}
